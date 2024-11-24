e4e917e9e469cc5210297bbfc6af5fd6627edceb
omdhaktode <dhaktodeom@gmail.com>
2024-11-24 23:30:51 +0530

    This is my information

---
+++ Information.java
@@ -0,0 +1,7 @@
+public class Information {
+    public static void main(String[] args) {
+        System.out.println("Name: Om Dhaktode");
+        System.out.println("Age: 21");
+        System.out.println("Address: Sanjiavani Kopargaon-423601");
+    }
+}
