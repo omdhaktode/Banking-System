001821042f0e7b18a292a0fcd2ea6ded5d037bfb
omdhaktode <dhaktodeom@gmail.com>
2024-11-25 23:40:20 +0530

    remove variabe

--- Hello.java
+++ Hello.java
@@ -2,8 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Om Dhaktode");
         System.out.println();
-        int a=10;
-        System.out.println(a);
     }
     
 }    
