3028b8f22697e0290af53774913036d395e78e34
omdhaktode <dhaktodeom@gmail.com>
2024-11-25 23:36:59 +0530

    It new Variable

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Om Dhaktode");
         System.out.println();
+        int a=10;
+        System.out.println(a);
     }
     
 }    
