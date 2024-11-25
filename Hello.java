0598200f09f22783f7f6a0e488c4f7fe862e8667
omdhaktode <dhaktodeom@gmail.com>
2024-11-25 23:26:12 +0530

    why

--- Hello.java
+++ Hello.java
@@ -2,5 +2,10 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Om Dhaktode");
         System.out.println();
+        int a=10;
+        int b=20;
+        System.out.println(a);
+        System.out.println(b);
+
     }
 }
