e7c2feadc9a4e65a80e539f9bb07302008808129
omdhaktode <dhaktodeom@gmail.com>
2024-11-25 22:48:10 +0530

    declare the variable

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Om Dhaktode");
         System.out.println();
+        int a=10;
+        System.out.println(a);
     }
-}
+}    
